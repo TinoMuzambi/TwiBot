386289a13b96b69c15863fef8b155ca0ca9f12e1
Tino <tinomuzambi@gmail.com>
2020-04-16 16:58:55 +0200

    Adds storing and retrieval of last seen id

--- twibot.py
+++ twibot.py
@@ -1,6 +1,6 @@
 import tweepy
 
-print("This is my Twitter bot.")
+FILE_NAME="last_seen_id.txt"
 
 CONSUMER_KEY = 'fQYLc7fQMBewZC6Z4tc56VTaJ'
 CONSUMER_SECRET = '4cqzZ5gMgfnbb10iBSWuNT0QlpVyCmat3Zuz3RR8YpdT6qvs3W'
@@ -10,3 +10,23 @@ ACCESS_SECRET = 'fOQUmWcD0iHswTyrD4wXDATvW16rXBOTkqq0mDnqGIqIo'
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
+
+def retrieve_last_seen_id(file_name):
+    f_read = open(file_name, 'r')
+    last_seen_id = int(f_read.read().strip())
+    f_read.close()
+    return last_seen_id
+
+def store_last_seen_id(last_seen_id, file_name):
+    f_write = open(file_name, 'w')
+    f_write.write(str(last_seen_id))
+    f_write.close()
+    return
+
+last_seen_id = retrieve_last_seen_id(FILE_NAME)
+mentions = api.mentions_timeline(last_seen_id, tweet_mode = "extended") 
+
+for mention in reversed(mentions):
+    print(str(mention.id) + " - " + mention.text)
+    if "#testing" in mention.text.lower():
+        print("Found #testing responding back")
